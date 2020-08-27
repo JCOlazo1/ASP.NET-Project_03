5d16e39e5e773cbb68091e4bdef9c11c4fce5525
jcolazo <jay.olazo@gmail.com>
2020-08-26 23:49:30 -0400

    Wire Up Login Form to API
    Connects the login form button to the authentication API endpoint (/token). Gets back the bearer token or an exception if failed.

--- JCDataManager/JCDesktopUI/Bootstrapper.cs
+++ JCDataManager/JCDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using JCDesktopUI.Helpers;
 using JCDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace JCDesktopUI
 {
@@ -16,6 +18,11 @@ namespace JCDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()
@@ -24,7 +31,8 @@ namespace JCDesktopUI
 
             _container
                 .Singleton<IWindowManager, WindowManager>()
-                .Singleton<IEventAggregator, EventAggregator>();
+                .Singleton<IEventAggregator, EventAggregator>()
+                .Singleton<IAPIHelper, APIHelper> ();
 
             GetType().Assembly.GetTypes().Where(type => type.IsClass)
                 .Where(type => type.Name.EndsWith("ViewModel"))
---
+++ JCDataManager/JCDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using JCDesktopUI.Models;
+using System.Threading.Tasks;
+
+namespace JCDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
